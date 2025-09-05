ccba4ba3b2e60494aa7cdeab35c3da18ebe70625
Renê Souza <rne_souza@yahoo.com.br>
2025-09-05 12:30:44 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/Rene1814/projeto-rsvendas" target="_blank" rel="noreferrer">Renê Souza</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Desenvolvido para agregar ao portfólio: <a href="https://github.com/Rene1814" target="_blank" rel="noreferrer">Github projects</a></small></p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
