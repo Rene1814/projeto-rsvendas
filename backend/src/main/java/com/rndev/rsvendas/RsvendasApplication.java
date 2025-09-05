89993bdf6bb06b626f58a2ed41407239951b0697
Renê Souza <rne_souza@yahoo.com.br>
2025-09-05 09:51:20 -0300

    Project created

---
+++ backend/src/main/java/com/rndev/rsvendas/RsvendasApplication.java
@@ -0,0 +1,13 @@
+package com.rndev.rsvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rndev/rsvendas/RsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rndev.rsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
