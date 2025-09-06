79bbf986371faf861005a2d32ee2d3a357634e45
Renê Souza <rne_souza@yahoo.com.br>
2025-09-06 18:52:12 -0300

    DonutChart integration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,9 @@
-import Dashboard from "pages/Dashboard";
-import Home from "pages/Home";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Routing from "Routing";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path = "/" element = {<Home/>}></Route>
-          <Route path = "/dashboard" element = {<Dashboard/>}></Route>
-        </Routes>
-      </BrowserRouter>
+      <Routing/>
     </>
   );
 }
