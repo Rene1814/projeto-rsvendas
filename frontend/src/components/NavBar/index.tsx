0bbe92b51256b8cfc6ffd098dfeac8436b4a70c6
Renê Souza <rne_souza@yahoo.com.br>
2025-09-06 10:10:47 -0300

    Home, navigation

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,13 +1,17 @@
 import ImgDsDark from 'assets/image/ImgDsDark.png'
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
         <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
             <div className="container">
-                <nav className="my-2 my-md-0 mr-md-3">
-                    <img src={ImgDsDark} alt="DevSuperior" width="200" height="70" />
+                <nav className="my-2 my-md-0 mr-md-3 ">
+                    <Link to="/">
+                        <img src={ImgDsDark} alt="DevSuperior" width="200" height="50" />
+                    </Link>
                 </nav>
             </div>
+
         </div>
     );
 }
