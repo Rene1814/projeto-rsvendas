5bd0b4f52c01b646c805acd1a9711b0f9548045a
Renê Souza <rne_souza@yahoo.com.br>
2026-09-08 08:38:01 -0300

    Project created

---
+++ rsvendas/src/test/java/com/rsdev/rsvendas/RsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsdev.rsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
