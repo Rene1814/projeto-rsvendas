c01564ac3e8edde87715ab0d6640bb36e052e819
Renê Souza <rne_souza@yahoo.com.br>
2026-09-08 10:28:56 -0300

    Static charts

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -14,20 +14,6 @@ const Footer = () => {
                 Renê Souza
               </a>
             </p>
-            <p className="text-light">
-              <small>
-                <strong>Semana Spring React</strong>
-                <br />
-                Evento promovido pela escola DevSuperior:{" "}
-                <a
-                  href="https://instagram.com/devsuperior.ig"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  @devsuperior.ig
-                </a>
-              </small>
-            </p>
           </div>
         </footer>
       </div>
