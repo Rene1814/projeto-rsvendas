9b83d79c25b3ced4fb9285b816033fb1166f5216
Renê Souza <rne_souza@yahoo.com.br>
2026-09-09 15:23:44 -0300

    Find seller by Id & insert seller

--- rsvendas/src/main/java/com/rsdev/rsvendas/controllers/SellerController.java
+++ rsvendas/src/main/java/com/rsdev/rsvendas/controllers/SellerController.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +29,17 @@ public class SellerController {
 	public ResponseEntity<List<SellerDTO>> findAll(){
 		List<SellerDTO> list = service.findAll();
 		return ResponseEntity.ok(list);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<SellerDTO> findById(@PathVariable Long id) {
+		SellerDTO result = service.findById(id);
+		return ResponseEntity.ok(result);
+	}
+
+	@PostMapping
+	public ResponseEntity<SellerDTO> insert(@RequestBody SellerDTO dto) {
+		SellerDTO result = service.insert(dto);
+		return ResponseEntity.ok(result);
 	}
 }
--- rsvendas/src/main/java/com/rsdev/rsvendas/services/SellerService.java
+++ rsvendas/src/main/java/com/rsdev/rsvendas/services/SellerService.java
@@ -3,7 +3,9 @@ package com.rsdev.rsvendas.services;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.rsdev.rsvendas.dto.SellerDTO;
 import com.rsdev.rsvendas.entities.Seller;
@@ -21,6 +23,19 @@ public class SellerService {
 	public List<SellerDTO> findAll(){
 		List<Seller> result = repository.findAll();
 		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+
+	public SellerDTO findById(Long id) {
+		Seller entity = repository.findById(id)
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Vendedor não encontrado"));
+		return new SellerDTO(entity);
+	}
+
+	public SellerDTO insert(SellerDTO dto) {
+		Seller entity = new Seller();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new SellerDTO(entity);
 	}
 	
 }
