5c3d03c4ce0859c4a436a63bab0f6cfdd344a9ed
Renê Souza <rne_souza@yahoo.com.br>
2026-09-09 15:58:16 -0300

    Create sale & find sale by Id

--- rsvendas/src/main/java/com/rsdev/rsvendas/controllers/SaleController.java
+++ rsvendas/src/main/java/com/rsdev/rsvendas/controllers/SaleController.java
@@ -7,6 +7,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,6 +33,18 @@ public class SaleController {
 	public ResponseEntity<Page<SaleDTO>> findAll(Pageable pageable){
 		Page<SaleDTO> list = service.findAll(pageable);
 		return ResponseEntity.ok(list);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<SaleDTO> findById(@PathVariable Long id) {
+		SaleDTO result = service.findById(id);
+		return ResponseEntity.ok(result);
+	}
+
+	@PostMapping
+	public ResponseEntity<SaleDTO> insert(@RequestBody SaleDTO dto) {
+		SaleDTO result = service.insert(dto);
+		return ResponseEntity.ok(result);
 	}
 	
 	@GetMapping(value = "/amount-by-seller")
--- rsvendas/src/main/java/com/rsdev/rsvendas/services/SaleService.java
+++ rsvendas/src/main/java/com/rsdev/rsvendas/services/SaleService.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.rsdev.rsvendas.dto.SaleDTO;
 import com.rsdev.rsvendas.dto.SaleSuccessDTO;
@@ -30,6 +32,26 @@ public class SaleService {
 		sellerRepository.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
+	}
+
+	@Transactional(readOnly = true)
+	public SaleDTO findById(Long id) {
+		Sale entity = repository.findById(id)
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Venda não encontrada"));
+		return new SaleDTO(entity);
+	}
+
+	@Transactional
+	public SaleDTO insert(SaleDTO dto) {
+		Sale entity = new Sale();
+		entity.setVisited(dto.getVisited());
+		entity.setDeals(dto.getDeals());
+		entity.setAmount(dto.getAmount());
+		entity.setDate(dto.getDate());
+		entity.setSeller(sellerRepository.findById(dto.getSeller().getId())
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Vendedor não encontrado")));
+		entity = repository.save(entity);
+		return new SaleDTO(entity);
 	}
 	
 	@Transactional(readOnly = true)
